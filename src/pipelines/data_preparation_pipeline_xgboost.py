7c0f22966327326485f6ac7d0b2d8d34692ddcdb
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-04 15:56:36 -0400

    v0.3.2 Created Column encoder

--- src/pipelines/data_preparation_pipeline_xgboost.py
+++ src/pipelines/data_preparation_pipeline_xgboost.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+import numpy as np
 from dataclasses import dataclass
 import pyspark
 from pyspark.sql import SparkSession
@@ -9,26 +11,70 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from typing import List
 
+from src.utils import read_delta
+from src.pipelines.cyclic_encoder import CyclicEncoder
+
 
 @dataclass
 class DataPreparationPipelineXGBoostConfig:
     root_delta_path: str = os.path.join("Data", "delta")
     gold_delta_path: str = os.path.join(root_delta_path, "gold")
     data_artifact_path: str = os.path.join("artifacts", "data")
-    time_feature_columns = [
-        "year",
-        "month",
-        "dayofmonth",
-        "weekday",
-        "weekofyear",
-        "dayofyear",
-        "hour",
-    ]
-    boolean_feature_columns = ["is_holiday"]
-    place_feature_columns = ["latitude", "longitude"]
-    label_columns = ["demand_bike", "demand_dock"]
+    categorical_column_names = ["weekday", "month", "is_holiday"]
+    numerical_column_names = ["latitude", "longitude", "year"]
+    cyclic_column_periods = {
+        "hourofday": 24,
+        "dayofmonth": 31,
+        "weekofyear": 52,
+        "dayofyear": 366,
+    }
 
 
 class DataPreparationPipelineXGboost:
     def __init__(self, spark: SparkSession):
         self.spark = spark
+        self.config = DataPreparationPipelineXGBoostConfig()
+
+    def get_column_transformer(self) -> ColumnTransformer:
+        num_pipeline = Pipeline(
+            steps=[
+                ("imputer", SimpleImputer(strategy="mean")),
+                ("scaler", StandardScaler()),
+            ],
+        )
+
+        cat_pipeline = Pipeline(
+            steps=[
+                ("imputer", SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder", OneHotEncoder()),
+                ("scaler", StandardScaler(with_mean=False)),
+            ]
+        )
+
+        cyclic_transformers = [
+            (
+                f"{name}_pipeline",
+                Pipeline(
+                    steps=[
+                        ("cyclic_encoder", CyclicEncoder(period)),
+                        ("scaler", StandardScaler()),
+                    ]
+                ),
+                [name],
+            )
+            for name, period in self.config.cyclic_column_periods.items()
+        ]
+
+        preprocessor = ColumnTransformer(
+            [
+                ("num_pipeline", num_pipeline, self.config.numerical_column_names),
+                ("cat_pipeline", cat_pipeline, self.config.categorical_column_names),
+            ]
+            + cyclic_transformers
+        )
+
+        return preprocessor
+    
+
+if __name__ == "__main__":
+    pipeline = DataPreparationPipelineXGboost(None)
--- test/pipelines/data_preparation_pipeline_xgboost_test.py
+++ test/pipelines/data_preparation_pipeline_xgboost_test.py
@@ -1,8 +1,29 @@
 import pytest
+from pyspark.sql import SparkSession
+from sklearn.compose import ColumnTransformer
 
 from src.pipelines.data_preparation_pipeline_xgboost import (
-    DataPreparationPipelineXGBoostConfig,
+    DataPreparationPipelineXGBoostConfig, DataPreparationPipelineXGboost,
 )
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder.master("local[1]")
+        .appName("local-tests")
+        .config("spark.executor.cores", "1")
+        .config("spark.executor.instances", "1")
+        .config("spark.sql.shuffle.partitions", "1")
+        .config("spark.driver.bindAddress", "127.0.0.1")
+        .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
+        .getOrCreate()
+    )
+    yield spark
+    spark.stop()
+
+@pytest.fixture
+def pipeline(spark: SparkSession):
+    return DataPreparationPipelineXGboost(spark)
 
 
 def test_config():
@@ -11,7 +32,16 @@ def test_config():
     assert hasattr(config, "root_delta_path")
     assert hasattr(config, "gold_delta_path")
     assert hasattr(config, "data_artifact_path")
-    assert hasattr(config, "time_feature_columns")
-    assert hasattr(config, "boolean_feature_columns")
-    assert hasattr(config, "place_feature_columns")
-    assert hasattr(config, "label_columns")
+    assert hasattr(config, "categorical_column_names")
+    assert hasattr(config, "numerical_column_names")
+    assert hasattr(config, "cyclic_column_periods")
+
+def test_init(pipeline: DataPreparationPipelineXGboost):
+    assert hasattr(pipeline, "spark")
+    assert hasattr(pipeline, "config")
+
+
+def test_get_column_transformer(pipeline: DataPreparationPipelineXGboost):
+    transformer = pipeline.get_column_transformer()
+
+    assert isinstance(transformer, ColumnTransformer)
