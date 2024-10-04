93ce0048f2691efabcbaaf3f82dfad16a92e056f
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-04 19:18:58 -0400

    Implemented cyclic encoding

--- src/components/silver_to_gold_transformer.py
+++ src/components/silver_to_gold_transformer.py
@@ -19,3 +19,23 @@ class SilverToGoldTransformer:
     def __init__(self, spark: SparkSession):
         self.spark = spark
         self.config = SilverToGoldTransformerConfig()
+
+    def cyclic_encode(self, df):
+        return (
+            df.withColumn(
+                "dayofmonth_sin", F.sin((2 * F.pi() * F.col("dayofmonth")) / 31)
+            )
+            .withColumn("dayofmonth_cos", F.cos((2 * F.pi() * F.col("dayofmonth")) / 31))
+            .withColumn("weekofyear_sin", F.sin((2 * F.pi() * F.col("weekofyear")) / 53))
+            .withColumn("weekofyear_cos", F.cos((2 * F.pi() * F.col("weekofyear")) / 53))
+            .withColumn("dayofyear_sin", F.sin((2 * F.pi() * F.col("dayofyear")) / 366))
+            .withColumn("dayofyear_cos", F.cos((2 * F.pi() * F.col("dayofyear")) / 366))
+            .withColumn("hour_sin", F.sin((2 * F.pi() * F.col("hour")) / 24))
+            .withColumn("hour_cos", F.cos((2 * F.pi() * F.col("hour")) / 24))
+            .drop("dayofmonth", "weekofyear", "dayofyear", "hour")
+        )
+
+    def transform(self):
+        df = read_delta(self.spark, self.config.silver_delta_path)
+
+        df = self.cyclic_encode(df)
--- test/components/silver_gold_transformer_test.py
+++ test/components/silver_gold_transformer_test.py
@@ -1,6 +1,9 @@
 import pytest
 from unittest.mock import Mock, patch
 from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+import pyspark.sql.types as T
+import pyspark.sql.functions as F
 
 from src.components.silver_to_gold_transformer import (
     SilverToGoldTransformerConfig,
@@ -29,6 +32,102 @@ def transformer(spark: SparkSession):
     return SilverToGoldTransformer(spark)
 
 
+@pytest.fixture
+def dataframe(spark: SparkSession):
+    df = spark.createDataFrame(
+        data=[
+            [
+                2,
+                0,
+                40.76727294921875,
+                -73.99392700195312,
+                2013,
+                6,
+                1,
+                5,
+                22,
+                152,
+                8,
+                True,
+            ],
+            [
+                1,
+                1,
+                40.76727294921875,
+                -73.99392700195312,
+                2013,
+                6,
+                1,
+                5,
+                22,
+                152,
+                11,
+                True,
+            ],
+            [
+                2,
+                4,
+                40.76727294921875,
+                -73.99392700195312,
+                2013,
+                6,
+                1,
+                5,
+                22,
+                152,
+                14,
+                True,
+            ],
+            [
+                3,
+                1,
+                40.76727294921875,
+                -73.99392700195312,
+                2013,
+                6,
+                1,
+                5,
+                22,
+                152,
+                15,
+                True,
+            ],
+            [
+                3,
+                4,
+                40.76727294921875,
+                -73.99392700195312,
+                2013,
+                6,
+                1,
+                5,
+                22,
+                152,
+                18,
+                True,
+            ],
+        ],
+        schema=T.StructType(
+            [
+                T.StructField("bike_demand", T.LongType(), True),
+                T.StructField("dock_demand", T.LongType(), True),
+                T.StructField("latitude", T.FloatType(), True),
+                T.StructField("longitude", T.FloatType(), True),
+                T.StructField("year", T.IntegerType(), True),
+                T.StructField("month", T.IntegerType(), True),
+                T.StructField("dayofmonth", T.IntegerType(), True),
+                T.StructField("weekday", T.IntegerType(), True),
+                T.StructField("weekofyear", T.IntegerType(), True),
+                T.StructField("dayofyear", T.IntegerType(), True),
+                T.StructField("hour", T.IntegerType(), True),
+                T.StructField("is_holiday", T.BooleanType(), True),
+            ]
+        ),
+    )
+
+    return df
+
+
 def test_config():
     config = SilverToGoldTransformerConfig()
 
@@ -43,3 +142,21 @@ def test_init(transformer: SilverToGoldTransformer, spark: SparkSession):
     assert transformer.spark is spark
     assert isinstance(transformer.config, SilverToGoldTransformerConfig)
     assert isinstance(transformer.spark, SparkSession)
+
+
+def test_cyclic_encode(transformer: SilverToGoldTransformer, dataframe: DataFrame):
+    expected = {
+        "dayofmonth_sin",
+        "dayofmonth_cos",
+        "weekofyear_sin",
+        "weekofyear_cos",
+        "dayofyear_sin",
+        "dayofyear_cos",
+        "hour_sin",
+        "hour_cos",
+    }
+    output = transformer.cyclic_encode(dataframe)
+
+    assert len(set(output.columns).intersection(expected)) == len(expected)
+
+    assert len(set(output.columns).intersection({"dayofmonth", "weekofyear", "dayofyear", "hour"})) == 0
