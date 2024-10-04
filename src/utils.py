724c6fece7d55ebe09cafaea6ac7c24abeee9b4c
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-04 18:59:06 -0400

    Save and load pickle objects done

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
+import pickle as pkl
 
 
 def read_delta(spark: SparkSession, path: str) -> DataFrame:
@@ -8,3 +9,17 @@ def read_delta(spark: SparkSession, path: str) -> DataFrame:
 
 def write_delta(df: DataFrame, path: str):
     df.write.save(path=path, format="delta", mode="overwrite")
+
+
+def save_as_pickle(obj, path: str):
+    with open(path, "wb") as file:
+        pkl.dump(obj, file)
+
+
+def read_pickle(path: str):
+    obj = None
+
+    with open(path, "rb") as file:
+        obj = pkl.load(file)
+
+    return obj
