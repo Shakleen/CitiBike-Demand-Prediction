b72aa081204c00315939e191f34a96e8c2b02886
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-05 11:54:59 -0400

    Get evaluator for xgboost pipeline

--- src/train_pipeline/xgboost_pipeline.py
+++ src/train_pipeline/xgboost_pipeline.py
@@ -2,6 +2,7 @@ import os
 from dataclasses import dataclass
 from pyspark.sql import SparkSession
 from xgboost.spark import SparkXGBRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
 
 from src.utils import read_delta
 
@@ -14,6 +15,11 @@ class XGBoostPipelineConfig:
     feature_column_name: str = "final_features"
     number_of_workers: int = 6
     device: str = "cuda"
+    bike_demand_column_name: str = "bike_demand"
+    dock_demand_column_name: str = "dock_demand"
+    bike_demand_prediction_column_name: str = "predicted_bike_demand"
+    dock_demand_prediction_column_name: str = "predicted_dock_demand"
+    evaluation_metric_name: str = "rmse"
 
 
 class XGBoostPipeline:
@@ -27,7 +33,26 @@ class XGBoostPipeline:
             label_col=label_column_name,
             num_workers=self.config.number_of_workers,
             device=self.config.device,
+            objective="reg:squarederror",
+        )
+
+    def get_evaluator(
+        self,
+        label_column_name: str,
+        predicted_column_name: str,
+    ) -> RegressionEvaluator:
+        return RegressionEvaluator(
+            predictionCol=predicted_column_name,
+            labelCol=label_column_name,
+            metricName=self.config.evaluation_metric_name,
         )
 
     def train(self):
         data = read_delta(self.spark, self.config.gold_delta_path)
+
+        regressor_bike_demand = self.get_xgboost_regressor(
+            self.config.bike_demand_column_name
+        )
+        regressor_dock_demand = self.get_xgboost_regressor(
+            self.config.dock_demand_column_name
+        )
--- test/train_pipeline/xgboost_pipeline_test.py
+++ test/train_pipeline/xgboost_pipeline_test.py
@@ -35,6 +35,10 @@ def test_config():
     assert hasattr(config, "feature_column_name")
     assert hasattr(config, "number_of_workers")
     assert hasattr(config, "device")
+    assert hasattr(config, "bike_demand_column_name")
+    assert hasattr(config, "dock_demand_column_name")
+    assert hasattr(config, "bike_demand_prediction_column_name")
+    assert hasattr(config, "dock_demand_prediction_column_name")
 
 
 def test_init(pipeline: XGBoostPipeline):
@@ -48,3 +52,18 @@ def test_get_xgboost_regressor(pipeline: XGBoostPipeline, label: str):
 
     assert regressor.getLabelCol() == label
     assert regressor.getFeaturesCol() == pipeline.config.feature_column_name
+
+
+@pytest.mark.parametrize(
+    ("predict_col", "label_col"),
+    [
+        ("bike_demand", "predicted_bike_demand"),
+        ("dock_demand", "predicted_dock_demand"),
+    ],
+)
+def test_get_evaluator(pipeline: XGBoostPipeline, predict_col: str, label_col: str):
+    evaluator = pipeline.get_evaluator(label_col, predict_col)
+
+    assert evaluator.getPredictionCol() == predict_col
+    assert evaluator.getLabelCol() == label_col
+    assert evaluator.getMetricName() == pipeline.config.evaluation_metric_name
