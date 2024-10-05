b170f19c8caff909e908050d99751c4ee4d5c9ff
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-05 11:32:22 -0400

    Installed xgboost and initialized pipeline

--- src/train_pipeline/xgboost_pipeline.py
+++ src/train_pipeline/xgboost_pipeline.py
@@ -1,8 +1,18 @@
 import os
 from dataclasses import dataclass
+from pyspark.sql import SparkSession
+
+from src.utils import read_delta
+
 
 @dataclass
 class XGBoostPipelineConfig:
     root_delta_path: str = os.path.join("Data", "delta")
     gold_delta_path: str = os.path.join(root_delta_path, "gold")
     model_artifact_path: str = os.path.join("artifacts", "model", "xgboost")
+
+
+class XGBoostPipeline:
+    def __init__(self, spark: SparkSession) -> None:
+        self.spark = spark
+        self.config = XGBoostPipelineConfig()
--- test/train_pipeline/xgboost_pipeline_test.py
+++ test/train_pipeline/xgboost_pipeline_test.py
@@ -1,6 +1,29 @@
 import pytest
+from unittest.mock import Mock, patch
+from pyspark.sql import SparkSession
 
-from src.train_pipeline.xgboost_pipeline import XGBoostPipelineConfig
+from src.train_pipeline.xgboost_pipeline import XGBoostPipelineConfig, XGBoostPipeline
+
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder.master("local[1]")
+        .appName("local-tests")
+        .config("spark.executor.cores", "1")
+        .config("spark.executor.instances", "1")
+        .config("spark.sql.shuffle.partitions", "1")
+        .config("spark.driver.bindAddress", "127.0.0.1")
+        .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
+        .getOrCreate()
+    )
+    yield spark
+    spark.stop()
+
+@pytest.fixture
+def pipeline(spark: SparkSession) -> XGBoostPipeline:
+    return XGBoostPipeline(spark)
+
 
 def test_config():
     config = XGBoostPipelineConfig()
@@ -8,3 +31,8 @@ def test_config():
     assert hasattr(config, "root_delta_path")
     assert hasattr(config, "gold_delta_path")
     assert hasattr(config, "model_artifact_path")
+
+
+def test_init(pipeline: XGBoostPipeline):
+    assert hasattr(pipeline, "spark")
+    assert hasattr(pipeline, "config")
