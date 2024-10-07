89062ccd5505d7682994ea037dad1c61bc21bfee
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-10-07 19:26:20 -0400

    Fill in station names in dropdown from csv

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, render_template
+import pandas as pd
 
+station_data = pd.read_csv(os.path.join("artifacts", "data", "Stations.csv"))
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     # Python list to populate the dropdown
-    options_list = ["Option 1", "Option 2", "Option 3", "Option 4"]
+    options_list = station_data.loc[:, "name"].tolist()
 
     # Pass the list to the HTML template
     return render_template("index.html", options=options_list)
